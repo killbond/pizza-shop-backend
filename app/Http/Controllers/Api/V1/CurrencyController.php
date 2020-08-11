758bf8a6ed416b9c1e3a46e7fa0d376cdf9c2c55
killbond <killbond@mail.ru>
2020-08-11 03:52:44 +0300

    added currency controller template and tests

---
+++ app/Http/Controllers/Api/V1/CurrencyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class CurrencyController
+ * @package App\Http\Controllers
+ */
+class CurrencyController extends Controller
+{
+    public function __invoke()
+    {
+        //
+    }
+}
