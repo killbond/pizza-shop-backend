63f141a1a1aeddc63764b3ccb3a60e7bdf58c3ae
killbond <killbond@mail.ru>
2020-08-11 05:14:03 +0300

    implemented ingredient controller

--- app/Http/Controllers/Api/V1/IngredientController.php
+++ app/Http/Controllers/Api/V1/IngredientController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\IngredientResource;
+use App\Ingredient;
 use Illuminate\Http\Request;
 
 class IngredientController extends Controller
 {
     public function __invoke(Request $request)
     {
-        //
+        return IngredientResource::collection(Ingredient::all());
     }
 }
