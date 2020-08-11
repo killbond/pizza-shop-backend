81e9a34e22079cd4ae252b6833361349c91dd62b
killbond <killbond@mail.ru>
2020-08-11 03:54:29 +0300

    implemented currency controller

--- app/Http/Controllers/Api/V1/CurrencyController.php
+++ app/Http/Controllers/Api/V1/CurrencyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Currency;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CurrencyResource;
 
 /**
  * Class CurrencyController
@@ -12,6 +14,6 @@ class CurrencyController extends Controller
 {
     public function __invoke()
     {
-        //
+        return CurrencyResource::collection(Currency::all());
     }
 }
