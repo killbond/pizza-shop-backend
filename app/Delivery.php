5bb79cf5bac0e87f61987a82819fc6b2700c7a69
killbond <killbond@mail.ru>
2020-08-13 23:14:42 +0300

    added delivery model and migrations

---
+++ app/Delivery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Delivery
+ *
+ * @method static Builder|Delivery newModelQuery()
+ * @method static Builder|Delivery newQuery()
+ * @method static Builder|Delivery query()
+ * @mixin Eloquent
+ */
+class Delivery extends Model
+{
+    //
+}
---
+++ database/migrations/2020_08_13_200614_create_coordinates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoordinatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coordinates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('delivery_id')
+                ->constrained('deliveries')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->string('address');
+            $table->decimal('lat', 10, 8);
+            $table->decimal('lng', 11, 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coordinates');
+    }
+}
