b6b1c6c4dbc24fe47b6c6940015efc858668986e
killbond <killbond@mail.ru>
2020-08-13 03:27:00 +0300

    added controller and routes

---
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    public function store()
+    {
+    }
+
+    public function list()
+    {
+    }
+}
